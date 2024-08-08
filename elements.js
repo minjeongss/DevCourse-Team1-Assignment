c6cf20b05470de806dcab5ec50eab1eaccaf95dd
jinhyukSeo777 <77399457+jinhyukSeo777@users.noreply.github.com>
2024-08-08 19:35:14 +0900

    .

--- elements.js
+++
@@ -1,4 +0,0 @@
-const $sliderUl = document.querySelector('.sliderUl');
-const $sliderDot = document.querySelector('.slider-dot');
-const [$prevBtn, $nextBtn] = document.querySelectorAll('.btn');
-export { $nextBtn, $prevBtn, $sliderDot, $sliderUl };
